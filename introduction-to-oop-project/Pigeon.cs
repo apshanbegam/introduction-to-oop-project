8bb4f36383d4a93b578f60a78a470a9620b69b18
apshanbegam <94931797+apshanbegam@users.noreply.github.com>
2022-05-13 21:41:35 +0100

    Initial commit

---
+++ introduction-to-oop-project/Pigeon.cs
@@ -0,0 +1,19 @@
+﻿//The Pigeon extends the base class Bird
+public class Pigeon : Bird
+{
+    //The Pigeon constructor calls the base class Bird constructor
+    //The name and color parameters are passed to the Bird constructor
+    public Pigeon(string name, string color) : base(name, color)
+    {
+    }
+
+    public void EatPizza()
+    {
+        Console.WriteLine("Delicious pizza!");
+    }
+
+    public void Sleep()
+    {
+        Console.WriteLine("I'm a sleeping pigeon");
+    }
+}
---
+++ introduction-to-oop-project/Program.cs
@@ -0,0 +1,10 @@
+﻿
+Bird myBird = new Bird("Tweety", "Yellow");
+myBird.Speak();
+myBird.Fly();
+
+Pigeon pippa = new Pigeon("Pippa", "Grey");
+pippa.Speak();
+pippa.Fly();
+pippa.EatPizza();
+pippa.Sleep();
